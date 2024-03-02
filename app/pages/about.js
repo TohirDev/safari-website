8f43bb97dece772e98232ebfd3189aa394610167
username-shakha <username.shakha@gmail.com>
2024-03-02 13:30:49 +0500

    change about

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function about() {
-  return <div>about</div>;
+  return (
+    <div>
+      <div>about</div>
+      <div>https://github.com/username-shakha/safari-website.git</div>
+    </div>
+  );
 }
 
 export default about;
